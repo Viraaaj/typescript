25c97bc506d992d8aafd182b45e991e6a6ac798c
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-06-29 19:06:12 +0530

    Complex types: arrays, objects in ts

--- basics-of-ts.ts
+++ basics-of-ts.ts
@@ -15,3 +15,39 @@ username = "John";
 let isSuperuser: boolean;
 isSuperuser = true;
 
+console.log("---------------------------Complex types in TS------------------------------");
+
+console.log("--------Arrays-------");
+//At the time of assigning array we need to write type and then []
+let stringsArray: string[];
+stringsArray = ["a", "b"]
+// stringsArray = ["a", "b", 12] //error
+
+let numbersArray: number[];
+numbersArray = [1,2]
+
+let booleansArray: boolean[];
+booleansArray = [true, false]
+
+console.log("--------Objects-------");
+let anyType: any; //This any will tell ts that you dont want to specify any type you can store anything in that variable
+
+let objectType: {
+    name: string;
+    age: number;
+}; //setting object type
+
+objectType = {
+    name: "john",
+    age: 10
+}
+
+// objectType = {
+//     isOld: true
+// } //Will give error because we havent specified boolean in objectType
+
+//Storing array of objects having having same elements
+let allData: {
+    name: string;
+    age: number;
+}[];
