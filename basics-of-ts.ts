f38d964ebfd664c9c946d5bee62c9e1c428b8859
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-06-29 18:55:11 +0530

    Base value/data types in ts

---
+++ basics-of-ts.ts
@@ -0,0 +1,17 @@
+//Primitives: numbers, strings, booleans: when declared this should be all in lowecases
+//comples types: arrays, objects
+// function types, parameters
+
+console.log("---------------------------Primitives in TS------------------------------");
+ 
+// let age:number = 12;
+let age: number;
+age = 12;
+// age = "12" //error
+
+let username:string
+username = "John";
+
+let isSuperuser: boolean;
+isSuperuser = true;
+
