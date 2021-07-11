413bd457719194f35711d294e1ee5cf4472a4246
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-07-11 19:04:59 +0530

    Error fixed in newTodo.tsx file while using refs

--- react-ts/src/components/newtodo.tsx
+++ react-ts/src/components/newtodo.tsx
@@ -1,22 +1,22 @@
 import { useRef } from "react";
 
 const Newtodo = () => {
-  //While using events in ts we need to set a type for event using Rect.type
-  const submitTodoHandlder = (event: React.FormEvent) => {
-    event.preventDefault();
-  };
-
   //While using refs we need to set where we are using ref: eg. input button etc also set starting value
   const todoTextInput = useRef<HTMLInputElement>(null);
 
-  //Here ? is telling ts that if there is any value then store in the enteredText otherwise store null
-  //If you are 100% sure that the value will never be null then you can use ! rather than ?
-  const enteredText = todoTextInput.current!.value;
+  //While using events in ts we need to set a type for event using Rect.type
+  const submitTodoHandlder = (event: React.FormEvent) => {
+    event.preventDefault();
 
-  if (enteredText.trim().length === 0) {
-    //Throw an error
-    return;
-  }
+    //Here ? is telling ts that if there is any value then store in the enteredText otherwise store null
+    //If you are 100% sure that the value will never be null then you can use ! rather than ?
+    const enteredText = todoTextInput.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      //Throw an error
+      return;
+    }
+  };
 
   return (
     <form onSubmit={submitTodoHandlder}>
